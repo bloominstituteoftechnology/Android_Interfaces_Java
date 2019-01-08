8c6b3aa60e957fc523130919e13e9cb3282e4675
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-08 11:50:47 -0800

    calls travel on each of the classes that extend the base class.

--- src/joshuahalvorson/com/Main.java
+++ src/joshuahalvorson/com/Main.java
@@ -1,9 +1,18 @@
 package joshuahalvorson.com;
 
+import java.util.ArrayList;
+
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-
+        ArrayList<Vehicle> vehicles = new ArrayList<>();
+        vehicles.add(new Car());
+        vehicles.add(new Boat());
+        vehicles.add(new Bicycle());
+        vehicles.add(new Plane());
+        vehicles.add(new Helicopter());
+        vehicles.add(new Seaplane());
+        for(Vehicle v : vehicles){
+            v.travel();
+        }
     }
 }
