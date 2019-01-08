cf0d15d6ea00e6c5f039e07a6156d82a1d4bd0a0
ThaDocIzN <rodan72@msn.com>
2019-01-08 16:30:34 -0600

    added a ski boat class

---
+++ Android_Interfaces_Java/src/com/company/SkiBoatClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class SkiBoatClass extends Vehicle implements WaterTravel {
+    @Override
+    public void travel() {
+
+    }
+
+    @Override
+    public void sail() {
+
+    }
+}
