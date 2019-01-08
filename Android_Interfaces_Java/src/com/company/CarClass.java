6933cdc679f04f5b88ddadb3836e1309dbcc9309
ThaDocIzN <rodan72@msn.com>
2019-01-08 16:28:10 -0600

    added a car class

---
+++ Android_Interfaces_Java/src/com/company/CarClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CarClass extends Vehicle implements DriveTravel {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void travel() {
+
+    }
+}
