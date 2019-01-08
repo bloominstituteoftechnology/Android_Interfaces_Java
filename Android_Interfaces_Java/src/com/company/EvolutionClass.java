d0651f71ec01a009b915653ae4651caa7529549d
ThaDocIzN <rodan72@msn.com>
2019-01-08 16:34:49 -0600

    added evolution class

---
+++ Android_Interfaces_Java/src/com/company/EvolutionClass.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class EvolutionClass extends Vehicle implements AirTravel, WaterTravel, DriveTravel {
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void travel() {
+
+    }
+
+    @Override
+    public void sail() {
+
+    }
+}
