957685390d53ce17bbda84c7461c31209592b9f5
ThaDocIzN <rodan72@msn.com>
2019-01-08 16:25:49 -0600

    added a tank class

---
+++ Android_Interfaces_Java/src/com/company/TankClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class TankClass extends Vehicle implements DriveTravel {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void travel() {
+
+    }
+}
