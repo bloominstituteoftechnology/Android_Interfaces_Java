4e73fab01dfe754587045fec0c91b81d95ea1a6a
ThaDocIzN <rodan72@msn.com>
2019-01-08 16:23:12 -0600

    added airplane class

---
+++ Android_Interfaces_Java/src/com/company/AirPlaneClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class AirPlaneClass extends Vehicle implements AirTravel {
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void travel() {
+
+    }
+}
