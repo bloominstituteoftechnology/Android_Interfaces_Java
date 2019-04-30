51033f2b8ffa7e1a92a677b48cbd2a6527a698f1
Shoonsk8 <shoon@att.net>
2019-04-30 15:29:40 -0400

    Now I understand what "The main method will create a list of Vehicles " means

--- app/src/main/java/com/shoon/android_interfaces_java/MainActivity.java
+++ app/src/main/java/com/shoon/android_interfaces_java/MainActivity.java
@@ -15,6 +15,19 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(hp.fly());
         tv.append("\n");
         AirTravel at=new Airplane();
-        tv.append(((Airplane) at).travel());
+        tv.append(((Airplane) at).travel()+"\n");
+        Vehicle[] vh=new Vehicle[6];
+        vh[0]=new Airplane();
+        vh[1]=new Boat();
+        vh[2]=new Bus();
+
+        vh[3]=new Car();
+        vh[4]=new Cruiser();
+        vh[5]=new Helicopter();
+        for(int i=0;i<6;i++){
+            tv.append(vh[i].travel()+"\n");
+        }
+
+
     }
 }
